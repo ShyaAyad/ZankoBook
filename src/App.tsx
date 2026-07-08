0f022ce0bd4d57c114b98b5d98582360ce895666
__rasyar <rasyar.safen19@gmail.com>
2026-07-08 15:23:56 +0300

    feat: implement login page (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "@/App.css";
 import AppLayout from "./Layout/AppLayout";
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
         <Route element={<AppLayout />}>
           <Route path="/" />
           <Route path="/requests" />
