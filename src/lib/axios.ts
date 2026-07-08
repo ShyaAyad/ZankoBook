75866c853cb77dc3eff3b29bfebab47c2bfce596
rasathelamedude <rasyar.safen19@gmail.com>
2026-07-08 12:34:28 +0300

    chore: set up axios and request / response interceptors

---
+++ src/lib/axios.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useUserStore } from "@/store/userStore";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Automatically attach token on each request
+api.interceptors.request.use(
+  (config) => {
+    const token = useUserStore.getState().token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Catch 401 (token expired or invalid) and redirect to login
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      const token = useUserStore.getState().token;
+      const clearAuth = useUserStore.getState().clearAuth;
+
+      // only react if we had tha token but it was no longer valid
+      if (token) {
+        clearAuth();
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
